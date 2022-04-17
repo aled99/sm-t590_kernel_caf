fe1d90e490915c12a719c689167035b74c91a046
aled99 <bumbi@live.it>
2022-04-17 10:30:08 +0200

    Revert "locking/rwsem: Get rid of __down_write_nested()"
    This reverts commit 68f97839735016174de0b09443b72ef1cc7bfa6c.

--- arch/s390/include/asm/rwsem.h
+++ arch/s390/include/asm/rwsem.h
@@ -115,7 +115,7 @@ static inline int __down_read_trylock(struct rw_semaphore *sem)
 /*
  * lock for writing
  */
-static inline void __down_write(struct rw_semaphore *sem)
+static inline void __down_write_nested(struct rw_semaphore *sem, int subclass)
 {
 	signed long old, new, tmp;
 
@@ -139,6 +139,11 @@ static inline void __down_write(struct rw_semaphore *sem)
 		: "cc", "memory");
 	if (old != 0)
 		rwsem_down_write_failed(sem);
+}
+
+static inline void __down_write(struct rw_semaphore *sem)
+{
+	__down_write_nested(sem, 0);
 }
 
 /*
